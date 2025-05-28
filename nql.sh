269a67b4b180a0c56ebf297b5922ae3d02c162a9
www1ktor <w.karykowski@icloud.com>
2025-05-28 23:50:45 +0200

    where finished

--- nql.sh
+++ nql.sh
@@ -37,7 +37,13 @@ parse_query() {
 
 		#echo "$RETURN_VAL"
 		if [[ "$RETURN_VAL" != "" ]]; then
-			PROMPT="$PROMPT$RETURN_VAL | "	
+			PROMPT="$PROMPT$RETURN_VAL"	
+			TEMP_FUNCTION=($FUNCTION)
+			echo $TEMP_FUNCTION
+			if [[ "${TEMP_FUNCTION[0]}" != "FROM" ]] && [[ "${TEMP_FUNCTION[0]}" != "WHERE" ]]; then
+				PROMPT="$PROMPT | "
+			fi
+
 			$FUNCTION
 		fi
         done
@@ -50,7 +56,7 @@ parse_query() {
 		eval $PROMPT
 	fi
 		
-	#echo "$PROMPT"
+	echo "$PROMPT"
 	
 }
 
@@ -62,7 +68,7 @@ FROM () {
 
         if [[ $(ls tables | grep -w $1) == "$1" ]]; then
                 TABLE_NAME=$1
-		echo "cat tables/$TABLE_NAME"
+		echo "cat tables/$TABLE_NAME | awk -F ';' "
 		return 0
 
         else
@@ -81,12 +87,11 @@ SELECT () {
 	fi
 	
 	if [[ "$@" == "\"*\"" ]] && [[ $N_O_COLUMNS -eq 1 ]]; then
-		echo "'awk -F ';' {print}'"
+		echo "{print}'"
 		return 0
 	fi
 
-	TABLE_COLUMNS=($(cat tables/$TABLE_NAME | head -1 | sed 's/;/ /g'))
-	
+	TABLE_COLUMNS=($(cat tables/$TABLE_NAME | head -1 | sed 's/;/ /g'))	       
 	COLUMNS_INDEXES=()
 
 	for IS_COLUMN in "${COLUMNS[@]}"; do
@@ -111,7 +116,7 @@ SELECT () {
 		fi
 	done
 
-	SEL_TEMP="'awk -F ';' {print"
+	SEL_TEMP="{print"
 
 	for COLUMN_INDEX in "${COLUMNS_INDEXES[@]}"; do
 		SEL_TEMP="$SEL_TEMP $COLUMN_INDEX\";\""
@@ -126,7 +131,51 @@ SELECT () {
 }
 
 WHERE () {
-	echo $@
+	#echo $@
+	#echo $#
+	local ARGS=($@)
+	WHR_TEMP="'\$1==\"ID\""
+
+	for arg in "${ARGS[@]}"; do
+		if [[ "${arg^^}" != "OR" ]] && [[ "${arg^^}" != "AND" ]]; then
+			eval $arg
+			
+			local COL_NAME="${arg%%=*}"
+			local COL_VALUE=""${!COL_NAME}""
+
+			#echo "$COL_NAME $COL_VALUE"
+		        
+			TABLE_COLUMNS=($(cat tables/$TABLE_NAME | head -1 | sed 's/;/ /g'))
+			INDEX=-1
+                	i=1
+
+                	for TABLE_COLUMN in "${TABLE_COLUMNS[@]}"; do
+                        	#echo $TABLE_COLUMN
+
+                        	if [[ "\"$TABLE_COLUMN\"" == "\"$COL_NAME\"" ]]; then
+					WHR_TEMP="$WHR_TEMP || \$$i==\"$COL_VALUE\""
+                                	INDEX=$i
+					break
+                        	fi
+
+                        	i=$(( $i + 1 ))
+                	done
+
+                	if [[ $INDEX -eq -1 ]]; then
+                        	echo "Syntax error! No column named $COL_NAME found in $TABLE_NAME"
+                        	return 1
+                	fi
+	
+		elif [[ "${arg^^}" == "OR" ]]; then 
+			WHR_TEMP="$WHR_TEMP || "	
+		
+		elif [[ "${arg^^}" == "AND" ]]; then
+			WHR_TEMP="$WHR_TEMP' | awk -F ';' "
+		fi
+	done
+	
+	echo $WHR_TEMP
+	
 }
 
 while [ true ]; do 
